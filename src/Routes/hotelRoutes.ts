e6f0f43f6ae9b334dc8228b68595a36aeda51332
linton <lintonli162@gmail.com>
2024-06-17 17:45:41 +0300

    fetched hotels by tourid

--- src/Routes/hotelRoutes.ts
+++ src/Routes/hotelRoutes.ts
@@ -1,11 +1,11 @@
 import { Router} from "express";
-import { addHotel, deleteHotel, getHotels, updateHotel } from "../Controllers/hotelController";
+import { addHotel, deleteHotel, getHotel, getHotelbyTour, getHotels, updateHotel } from "../Controllers/hotelController";
 import { verifyTokens } from "../middlewares";
 const hotelRoute = Router()
 
 hotelRoute.get("",getHotels)
-hotelRoute.get("/hotel/:id", verifyTokens,getHotels)
-hotelRoute.get("/tour/:id", verifyTokens, getHotels);
+hotelRoute.get("/hotel/:id", verifyTokens, getHotel)
+hotelRoute.get("/tour/:id", verifyTokens, getHotelbyTour);
 hotelRoute.post("", verifyTokens, addHotel);
 hotelRoute.patch("/:id", verifyTokens, updateHotel);
 hotelRoute.delete("/:id", verifyTokens, deleteHotel);
